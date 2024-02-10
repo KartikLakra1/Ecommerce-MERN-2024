02f8765f549e440def54af7a8a835983e80bc058
KartikLakra1 <kitrakarkal@gmail.com>
2024-02-10 21:23:57 +0530

    Product API CRUD created

--- controllers/productController.js
+++ controllers/productController.js
@@ -121,3 +121,77 @@ export const productPhotoController = async (req, res) => {
         })
     }
 }
+
+export const deleteProductController = async (req, res) => {
+    try {
+        await ProductModel.findByIdAndDelete(req.params.pid).select("-photo")
+        res.status(201).send({
+            success: true,
+            message: "Product deleted successfully"
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: "Error in deletion of product"
+        })
+    }
+}
+
+export const updateProductController = async (req, res) => {
+    try {
+        const { name, slug, description, price, category, quantity, shipping } = req.fields;
+        const { photo } = req.files;
+
+        // Validation
+        switch (true) {
+            case !name:
+                return res.status(500).send({ error: "Name is required" });
+
+            case !description:
+                return res.status(500).send({ error: "description is required" });
+
+            case !price:
+                return res.status(500).send({ error: "price is required" });
+
+            case !category:
+                return res.status(500).send({ error: "category is required" });
+
+            case !quantity:
+                return res.status(500).send({ error: "Qunatity is required" });
+
+            case photo && photo.size > 1000000:
+                return res.status(500).send({ error: "Photo is required and should be less than 1mb" });
+
+
+        }
+
+        const products = await ProductModel.findByIdAndUpdate(req.params.pid, {
+            ...req.fields, slug: slugify(name)
+        }, {
+            new: true
+        })
+
+        if (photo) {
+            products.photo.data = fs.readFileSync(photo.path);
+            products.photo.contentType = photo.type
+        }
+
+        await products.save();
+
+        res.status(201).send({
+            success: true,
+            message: "Product created successfully",
+            products
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: "Error in product updation"
+        })
+    }
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAdmin, requireSignIn } from './../MiddleWare/authMiddleware.js';
-import { createProductController, getProductController, getSingleProductController, productPhotoController } from '../controllers/productController.js';
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController, updateProductController } from '../controllers/productController.js';
 import formidable from 'express-formidable';
 
 const router = express.Router();
@@ -16,5 +16,11 @@ router.get('/get-product/:slug', getSingleProductController);
 
 //get photo
 router.get('/product-photo/:pid', productPhotoController);
+
+// delete product controller
+router.delete('/delete-product/:pid', deleteProductController);
+
+// update product
+router.put('/update-product/:pid', requireSignIn, isAdmin, formidable(), updateProductController);
 
 export default router;
