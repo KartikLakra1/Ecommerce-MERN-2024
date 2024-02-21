07d1beb11501470c70431de6a8bbc622b0bf2dc9
KartikLakra1 <kitrakarkal@gmail.com>
2024-02-21 21:04:59 +0530

    home page ui

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css"
 import Layout from './../../Components/Layout/Layout';
 import { useAuth } from "../../Context/Auth";
+import axios from 'axios';
 
 const Home = () => {
     const [auth, setAuth] = useAuth();
+    const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
+
+    // get all products
+    const getallProducts = async () => {
+        try {
+            const { data } = await axios.get('/api/v1/product/get-product');
+            setProducts(data.products);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <Layout>
-            <h1>Home</h1>
-            <pre>{JSON.stringify(auth, null, 4)}</pre>
+            <div className="row mt-3">
+                <div className="col-md-3">
+                    <h4 className="text-center">Filter By Category</h4>
+                </div>
+                <div className="col-md-9">
+                    <h1 className="text-center">All Products</h1>
+                    <div className="d-flex flex-wrap">
+                        <h1>Products</h1>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 };
