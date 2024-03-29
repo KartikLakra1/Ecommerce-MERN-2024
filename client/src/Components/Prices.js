f41ec64fa0fe09db346f8d089b7105f91f7cc167
KartikLakra1 <kitrakarkal@gmail.com>
2024-03-29 20:44:39 +0530

    filter category

--- client/src/Components/Prices.js
+++ client/src/Components/Prices.js
@@ -1,19 +1,19 @@
 export const Prices = [
     {
         _id: 0,
-        name: "$0-39",
-        array: [0, 39]
+        name: "$0-99",
+        array: [0, 99]
     }, {
         _id: 1,
-        name: "$40-79",
-        array: [40, 79]
+        name: "$100-399",
+        array: [100, 399]
     }, {
         _id: 2,
-        name: "$80-99",
-        array: [80, 99]
+        name: "$400-899",
+        array: [400, 899]
     }, {
         _id: 3,
-        name: "$100-onwards",
-        array: [100, 9999]
+        name: "$900-onwards",
+        array: [900, 9999]
     }
 ]
